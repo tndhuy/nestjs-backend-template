68cbfc3b9fe2b4ed588fc3afcb874fa17415f975
Tran Nguyen Dang Huy <trannguyendanhuy2904@gmail.com>
2026-04-11 17:36:47 +0700

    feat(interceptor): automatically wrap all responses in standard envelope

--- packages/create-app/templates/mongo/src/common/interceptors/transform.interceptor.ts
+++ packages/create-app/templates/mongo/src/common/interceptors/transform.interceptor.ts
@@ -2,7 +2,6 @@ import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from '@nes
 import { Reflector } from '@nestjs/core';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
-import { PUBLIC_API_KEY } from '../decorators/public-api.decorator';
 
 export const RAW_RESPONSE_KEY = 'raw_response';
 
@@ -20,17 +19,7 @@ export class TransformInterceptor<T> implements NestInterceptor<T, unknown> {
       return next.handle();
     }
 
-    const isPublic = this.reflector.getAllAndOverride<boolean>(PUBLIC_API_KEY, [
-      context.getHandler(),
-      context.getClass(),
-    ]);
-
-    // Internal first: If not explicitly marked as Public API, return RAW data
-    if (!isPublic) {
-      return next.handle();
-    }
-
-    // Public API: Wrap response in a standardized success object
+    // Wrap all responses in a standardized success object
     return next.handle().pipe(
       map((data) => {
         // Handle paginated results: { items: [], meta: {} }
